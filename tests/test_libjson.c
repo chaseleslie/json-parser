e189838cf2f78a9dcff31d9907891543bcb25187
Chase <sharmalay@users.noreply.github.com>
2016-06-05 02:37:46 -0400

    Add/fix autotools tests

--- tests/test_libjson.c
+++ tests/test_libjson.c
@@ -1,9 +1,15 @@
 #include "../src/json.h"
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+static void exit_failure(int ret) {
+	exit(ret);
+}
+
 int main() {
+	int retVal = 0;
 	const char* jsonStr = (
 		"{"
 		"\"obj\": {\"arr\": [1,2,3]},"
@@ -15,13 +21,28 @@ int main() {
 		"}"
 	);
 	const size_t jsonStrLen = strlen(jsonStr);
-	char buf[BUFSIZ];
-	setbuf(stderr, buf);
 	
 	json_parser_state* parserState = json_parser_init(NULL, NULL);
+	if (!parserState) {
+		retVal = 1;
+		exit_failure(retVal);
+	}
 	json_value* topVal = json_parser_parse(parserState, jsonStr, jsonStrLen);
+	if (!topVal) {
+		retVal = 1;
+		exit_failure(retVal);
+	}
 	
-	//Buffer must be valid when stream closed
+	char buf[BUFSIZ];
+	setbuf(stderr, buf);
+	parserState->jsonStrPos = 26u;
+	json_error_lineno("%zu:%zu", parserState);
+	if (strncmp("0:26", buf, 4)) {
+		retVal = 1;
+		fclose(stderr);
+		exit_failure(retVal);
+	}
+	//Buffer must be valid when stream closed (setbuf)
 	fclose(stderr);
-	return 0;
+	return retVal;
 }
